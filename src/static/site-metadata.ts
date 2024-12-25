bc03c32702819cb5bba41ee7f12bec2f07a07939
luoxihao <luoxihao2001@outlook.com>
2024-12-25 16:19:05 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -12,7 +12,7 @@ interface ISiteMetadataResult {
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
   siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  logo: 'assets/lxh.jpg',
   description: 'Personal site and blog',
   navLinks: [
     {
